a108efd11cd31d01f9733dee2da8e1156fc853bb
Leo Wilkin <leo@wilkin.xyz>
2025-01-31 16:17:58 +0000

    add readme and change config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,16 +4,24 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://myday.wilkin.xyz',
 	integrations: [
 		starlight({
 			title: 'myDay Docs',
 			social: {
 				github: 'https://github.com/leowilkin/myday',
 			},
+			editLink: {
+				baseUrl: 'https://github.com/leowilkin/myday/edit/main/docs/',
+			  },
 			sidebar: [
 				{
 					label: 'Guides',
 					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
 				},
 			],
 		}),
